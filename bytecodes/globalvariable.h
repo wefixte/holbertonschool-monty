737c6baa5dada1c3f1ed9d3d9387362912090402
wefixte <cassy.pernelle@gmail.com>
2024-01-03 14:05:29 +0100

    Remove globalvariable.h and update nop.c

--- bytecodes/globalvariable.h
+++
@@ -1,6 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-extern stack_t *stack;
-
-#endif
--- nop.c
+++ nop.c
@@ -4,7 +4,6 @@
  * nop - do nothing
  * @stack: stack
  * @line_number: line number
- * Return: void
  */
 
 void nop(stack_t **stack, unsigned int line_number)
