45f706ba7ed6956c61603c5eadcfb240a7256982
wefixte <cassy.pernelle@gmail.com>
2024-01-03 10:05:57 +0100

    Correction tabs

--- monty.h
+++ monty.h
@@ -24,9 +24,9 @@ int main(int argc, char *argv[]);
 
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -40,8 +40,8 @@ typedef struct stack_s
 
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 #endif /* MONTY_H */
